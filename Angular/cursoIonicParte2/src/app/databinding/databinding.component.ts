bbaa6140f349d30a8427945ac3993dabdd5c8221
Edward Alexander Gómez Ispanel <edward25nov@gmail.com>
2017-03-10 19:34:39 -0600

    clase angular 2 - types of binding

---
+++ Angular/cursoIonicParte2/src/app/databinding/databinding.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+  @Input()
+  result:number =0;  // decorador input
+
+  @Input()
+  hola:string;
+
+  stringInterpolation:string = "hola";
+  numberInterpolation:number = 1993;
+
+  imagen:string = "https://raw.githubusercontent.com/ninya-io/ninya-identity/master/logo/ninya-mascot@2x.png";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onclicked(event){
+    alert("funciona!!!");
+    console.log(event);
+  }
+
+  persona ={
+    nombre :"edward",
+    edad:23
+  };
+}
