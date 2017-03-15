fe56d329047d0cdacddc19b220aa206f6e2be61d
José Cabrera <deltadax18@gmail.com>
2017-03-14 19:46:48 -0600

    Attributes Directive

--- Angular-Master/cursoIonicParte3/src/app/highlight.directive.js
+++ Angular-Master/cursoIonicParte3/src/app/highlight.directive.js
@@ -7,11 +7,58 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require('@angular/core');
 var HighlightDirective = (function () {
+    // constructor(private elementRef : ElementRef, private renderer : Renderer) {
     function HighlightDirective() {
+        // Changing attributes, the bad way
+        // this.elementRef.nativeElement.style.backgroundColor = "green";
+        // Changing attibutes, the correct way
+        // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+        this.defaultColor = "white";
+        this.highlightColor = "green";
     }
+    //TODO: Listening to Events
+    HighlightDirective.prototype.mouseover = function () {
+        // this.backgroundColor = "green";
+        this.backgroundColor = this.highlightColor;
+    };
+    HighlightDirective.prototype.mouseleave = function () {
+        // this.backgroundColor = "white";
+        this.backgroundColor = this.defaultColor;
+    };
+    HighlightDirective.prototype.onClick = function (event) {
+        console.log("Event Target" + event.target);
+    };
+    Object.defineProperty(HighlightDirective.prototype, "setColor", {
+        get: function () {
+            return this.backgroundColor;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    HighlightDirective.prototype.ngOnInit = function () {
+        this.backgroundColor = this.defaultColor;
+    };
+    __decorate([
+        core_1.HostListener('mouseenter')
+    ], HighlightDirective.prototype, "mouseover");
+    __decorate([
+        core_1.HostListener('mouseleave')
+    ], HighlightDirective.prototype, "mouseleave");
+    __decorate([
+        core_1.HostListener('click', ['$event'])
+    ], HighlightDirective.prototype, "onClick");
+    __decorate([
+        core_1.HostBinding('style.backgroundColor')
+    ], HighlightDirective.prototype, "setColor");
+    __decorate([
+        core_1.Input()
+    ], HighlightDirective.prototype, "defaultColor");
+    __decorate([
+        core_1.Input("highlight")
+    ], HighlightDirective.prototype, "highlightColor");
     HighlightDirective = __decorate([
         core_1.Directive({
-            selector: '[appHighlight]'
+            selector: '[highlight]'
         })
     ], HighlightDirective);
     return HighlightDirective;
