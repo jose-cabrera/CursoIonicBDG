5cc9600adcb37367ca23ad62d6a4f95c1a749bd9
José Cabrera <deltadax18@gmail.com>
2017-03-15 21:01:27 -0600

    Commit despues de la clase

--- Angular-Master/cursoIonicParte3/src/app/app.component.ts
+++ Angular-Master/cursoIonicParte3/src/app/app.component.ts
@@ -8,6 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 
+  variable = true;
+
+  getClass(){
+    return {
+      'myClass' : this.variable
+    }
+  }
+
+  color:string = "black";
+
+  getStyle(){
+    return  {
+      'background-color' : this.color
+    }
+  }
+
   //*ngIf
   private switch : boolean = true;
 
@@ -20,6 +36,6 @@ export class AppComponent {
   private items = [1,2,3,4,5,6,7,8];
 
   //ngSwith
-  value : number = 0;
+  value : number = 20;
 
 }
--- Angular-Master/cursoIonicParte3/src/app/highlight.directive.js
+++ Angular-Master/cursoIonicParte3/src/app/highlight.directive.js
@@ -7,30 +7,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require('@angular/core');
 var HighlightDirective = (function () {
-    // constructor(private elementRef : ElementRef, private renderer : Renderer) {
     function HighlightDirective() {
-        // Changing attributes, the bad way
-        // this.elementRef.nativeElement.style.backgroundColor = "green";
-        // Changing attibutes, the correct way
-        // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+        //
         this.defaultColor = "white";
+        //
         this.highlightColor = "green";
+        //
+        this.backgroundColor = this.defaultColor;
     }
-    //TODO: Listening to Events
+    // //TODO: Listening to Events
     HighlightDirective.prototype.mouseover = function () {
         // this.backgroundColor = "green";
         this.backgroundColor = this.highlightColor;
     };
+    //
     HighlightDirective.prototype.mouseleave = function () {
         // this.backgroundColor = "white";
         this.backgroundColor = this.defaultColor;
     };
-    HighlightDirective.prototype.onClick = function (event) {
-        console.log("Event Target" + event.target);
-    };
     Object.defineProperty(HighlightDirective.prototype, "setColor", {
         get: function () {
-            return this.backgroundColor;
+            return 'myClass';
         },
         enumerable: true,
         configurable: true
@@ -45,16 +42,13 @@ var HighlightDirective = (function () {
         core_1.HostListener('mouseleave')
     ], HighlightDirective.prototype, "mouseleave");
     __decorate([
-        core_1.HostListener('click', ['$event'])
-    ], HighlightDirective.prototype, "onClick");
-    __decorate([
-        core_1.HostBinding('style.backgroundColor')
+        core_1.HostBinding('class')
     ], HighlightDirective.prototype, "setColor");
     __decorate([
-        core_1.Input()
+        core_1.Input('defaultColor')
     ], HighlightDirective.prototype, "defaultColor");
     __decorate([
-        core_1.Input("highlight")
+        core_1.Input('highlight')
     ], HighlightDirective.prototype, "highlightColor");
     HighlightDirective = __decorate([
         core_1.Directive({
