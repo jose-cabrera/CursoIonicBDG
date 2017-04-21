6a5d1e984e0461aee7a83a2d8c879e43e68da9a6
José Cabrera <deltadax18@gmail.com>
2017-04-20 20:17:31 -0600

    feat: added pipes proyect

---
+++ Angular-Master/Pipes/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AsyncPromisePipeComponentComponent } from './async-promise-pipe-component/async-promise-pipe-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AsyncPromisePipeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
